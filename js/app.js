59293b172b2a8204f21d83cd448ab1fcfe43b05d
Ezell Frazier <lynellf@gmail.com>
2017-12-08 07:05:13 -0600

    enabled clicking of transcript to change video stamp, adjusted breakpoints

--- js/app.js
+++ js/app.js
@@ -1,66 +1,60 @@
-const video = $('video');
-const player = document.querySelector('video');
-const tracks = document.querySelectorAll('.track');
-const time = player.currentTime;
-const chkpt = {
+const video = $('video'),
+player = document.querySelector('video'),
+tracks = $('.track'),
+chkpt = {
   start: [
-    0.000, 4.130, 7.535,
-    11.270, 13.960, 17.940,
-    22.370, 26.880, 30.920,
-    32.100, 34.730, 39.430,
-    41.190, 42.350, 46.300,
-    49.270, 53.760, 57.780
+    0.240, 4.130, 7.535, 11.270, 
+    13.960, 17.940, 22.370, 26.880, 
+    32.100, 34.730, 39.430, 42.350, 
+    46.300, 49.270, 53.760, 57.780
   ],
   end: [
-    4.130, 7.535, 11.270,
-    13.960, 17.940, 22.370,
-    26.880, 30.920, 32.100,
-    34.730, 39.430, 41.190,
-    42.350, 46.300, 49.270,
-    53.760, 57.780, 60.150
+    4.130, 7.535, 11.270, 13.960, 
+    17.940, 22.370, 26.880, 30.920, 
+    34.730, 39.430, 41.190, 46.300, 
+    49.270, 53.760, 57.780, 60.150
   ]
-}
+};
+
 video.mediaelementplayer({
-    features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen']
+    features: [
+        'playpause', 'current', 'progress', 
+        'duration', 'tracks', 'volume', 'fullscreen'
+    ],
+    videoWidth: '100%',
+    videoHeight: '100%',
+    enableAutosize: true
 });
+
+// Highlight transcript text as video plays.
+// The start and end checkpoints of the video transcript are
+// matched to the index position of each track class.
+// For as long as the timestamp falls between the start and end
+// checkpoint, the track class shall be modified to change state.
+
+// jQuery selectors did not function as intended for toggling the
+// className of each track in the array. Thus, I resorted to vanilla JS.
+
 player.ontimeupdate = () => {
-    // if(player.currentTime > 0 && player.currentTime <=  4.130) {
-    //     tracks[0].className += ' active';
-    // } else {
-    //     tracks[0].className = 'track';
-    // }
-    // if(player.currentTime > 4.130 && player.currentTime <=  7.535) {
-    //     tracks[1].className += ' active';
-    // } else {
-    //     tracks[1].className = 'track';
-    // }
-    // if(player.currentTime > 7.535 && player.currentTime <=  11.270) {
-    //     tracks[2].className += ' active';
-    // } else {
-    //     tracks[2].className = 'track';
-    // }
-    // if(player.currentTime > 11.270 && player.currentTime <=  13.960) {
-    //     tracks[3].className += ' active';
-    // } else {
-    //     tracks[3].className = 'track';
-    // }
-    // if(player.currentTime > 13.960 && player.currentTime <=  17.940) {
-    //     tracks[4].className += ' active';
-    // } else {
-    //     tracks[4].className = 'track';
-    // }
-    // if(player.currentTime > 7.535 && player.currentTime <=  11.270) {
-    //     tracks[2].className += ' active';
-    // } else {
-    //     tracks[2].className = 'track';
-    // }
+    const time = player.currentTime,
+    start = chkpt.start,
+    end = chkpt.end;
 
     for (let i = 0; i < tracks.length; i++) {
-      let prevTrk= i-1;
-      if(player.currentTime > chkpt.start[i] && player.currentTime <= chkpt.end[i]) {
+      if(time > start[i] && time < end[i]) {
         tracks[i].className += ' active';
       } else {
         tracks[i].className = 'track';
       }
     }
 }
+
+// Enable clicking of transcript to 
+// change the currentTime of the video.
+
+tracks.on( 'click', (event)=> {
+    const pos = tracks.index(event.target),
+    time = player.currentTime,
+    start = chkpt.start;
+    player.currentTime = start[pos];
+});
